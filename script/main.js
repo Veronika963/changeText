dd1933b13e79cfad26b3d23eb4f74f9f24bf9cac
Veronika963 <nicolewatter7on@gmail.com>
2023-01-14 16:19:17 +0200

    change text

--- script/main.js
+++ script/main.js
@@ -4,3 +4,8 @@ text.addEventListener('mouseenter', () => {
     text.innerHTML = 'раша параша';
     text.style.color = '#008080'; 
 });
+
+text.addEventListener('mouseout', () => {
+    text.innerHTML = 'Привіт світ';
+    text.style.color = '#800080'; 
+});
